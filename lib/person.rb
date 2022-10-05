bd54d10fb3a9be9b8a5dc8dcc3f57aa10dfc6a9e
monginadiana <dianahmongina2@gmail.com>
2022-10-05 08:35:33 +0300

    commit tree 9a56a9c60df158fd8dac70994776b30810fa07b0

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-  # your code here
+
+  def initialize(args)
+    args.each do |k,v|
+    self.class.attr_accessor(k)
+    self.send("#{k}=",v)
+    # binding.pry
+    end
+  end
 end
